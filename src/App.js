8ba57a50ac393bf853b184e31d6f2c5648b4b592
Carlos  Acuña <c.andres.a30@gmail.com>
2021-03-11 16:05:49 -0500

    Agregando menú índice y todos los componentes hasta el momento.

--- src/App.js
+++ src/App.js
@@ -1,64 +1,52 @@
-import React, { useState } from 'react';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import CountDownTimer from './components/countDownTimer';
+import SelectStorage from './components/selectStorage';
 
 const App = () => {
-  const [secondsParam, setSecondsParam] = useState(120);
-  const [dateTimestamp, setDateTimestamp] = useState('');
-  const [counterTimer, setCounterTimer] = useState('');
-  const [timerInterval, setTimerInterval] = useState('');
-  const [incrementalState, setIncrementalState] = useState(false);
-
-  const calculateTime = () => {
-    let initalSeconds = Number(secondsParam);
-
-    // Modified seconds to minutes
-    var secondsToMinutes = Math.floor(initalSeconds / 60);
-    var dateNow = new Date();
-
-    // Modified form date
-    let milliseconds = Number(dateTimestamp.slice(0,10)) * 1000;
-    var dateOriginal = new Date(milliseconds);    
-    var newDate  = new Date(dateOriginal);
-
-    newDate.setMinutes(dateOriginal.getMinutes() + secondsToMinutes);
-
-    // Generate new date timer value
-    let timeDiff = Math.abs(newDate.getTime() - dateNow.getTime());
-    // Validate state incremental counter
-    newDate.getTime() < dateNow.getTime() ? setIncrementalState(true) : setIncrementalState(false);
-
-    var secDiff = Math.floor((timeDiff / 1000) % 60);
-    var minDiff = Math.floor((timeDiff / 60 / 1000) % 60);
-    var hDiff = Math.floor(timeDiff / 3600 / 1000);
-    secDiff = secDiff < 10 ? '0'+secDiff : secDiff
-    minDiff = minDiff < 10 ? '0'+minDiff : minDiff
-    hDiff = hDiff == 0 ? '' : hDiff < 10  ? '0'+hDiff+':' : hDiff+':'
-    let timer = `${hDiff}${minDiff}:${secDiff}`;
-    setCounterTimer(timer);
-  }
-
-  const counter = async () => {
-    clearInterval(timerInterval);
-    await calculateTime();
-    setTimerInterval(setInterval(async () => await calculateTime(), 1000));
-  }
-
   return (
-    <div className="App">
+    <Router>
       <div>
-        <label>Segundos adicionales: </label>
-        <input type="number" value={secondsParam} onChange={(e) => setSecondsParam(e.target.value)} />
-        <br />
-        <label>Unix Timestamp: </label>
-        <input type="number" value={dateTimestamp} onChange={(e) => setDateTimestamp(e.target.value)} />
-        {secondsParam > 0 &&
-          <div>
-            <button onClick={counter}>Iniciar</button>
-            <br />
-            <span style={incrementalState ? { color: '#FF0000' } : { color: '#000000' }}>{counterTimer}</span>
-          </div>
-        }
+        <div style={{ float: 'left', width: '20%' }}>
+          <ul>
+            <li>
+              <Link to="/">Inicio</Link>
+            </li>
+            <li>
+              <Link to="/countdowntimer">CountDownTimer</Link>
+            </li>
+            <li>
+              <Link to="/selectstorage">Selects storage</Link>
+            </li>
+          </ul>
+        </div>
+        <div style={{ float: 'left', width: '80%', paddingTop: 20 }} >
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/countdowntimer">
+              <CountDownTimer />
+            </Route>
+            <Route path="/selectstorage">
+              <SelectStorage />
+            </Route>
+          </Switch>
+        </div>
       </div>
+    </Router>
+  )
+}
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Inicio: seleccione una opción.</h2>
     </div>
   );
 }
